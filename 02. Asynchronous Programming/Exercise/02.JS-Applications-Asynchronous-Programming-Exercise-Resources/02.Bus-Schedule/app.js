41a4c298314758982033c00c4fb963de79a04a41
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2024-02-28 23:25:49 +0200

    Second task compleated

--- 02. Asynchronous Programming/Exercise/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ 02. Asynchronous Programming/Exercise/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -1,11 +1,38 @@
 function solve() {
+    let departButton = document.getElementById("depart");
+    let arriveButton = document.getElementById("arrive");
+    let textField = document.querySelector("#info span");
+    // departButton.addEventListener("click", depart);
+    // arriveButton.addEventListener("click", arrive);
+    let url = "http://localhost:3030/jsonstore/bus/schedule/"
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stop = {
+        currentStop: "",
+        nextStop: "depot"
+    }
+
+
+    async function depart() {
+        try {
+            let response = await fetch(url + stop.nextStop);
+            let data = await response.json();
+            stop.currentStop = data.name;
+            stop.nextStop = data.next;
+            textField.textContent = `Next stop ${stop.currentStop}`;
+            departButton.disabled = true;
+            arriveButton.disabled = false;
+        } catch (error) {
+            textField.textContent = "Error";
+            departButton.disabled = true;
+            arriveButton.disabled = true;
+        }
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        textField.textContent = `Arriving at ${stop.currentStop}`;
+        departButton.disabled = false;
+        arriveButton.disabled = true;
     }
 
     return {
