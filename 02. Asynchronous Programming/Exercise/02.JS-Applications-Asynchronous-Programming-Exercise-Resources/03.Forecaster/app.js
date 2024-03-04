f1e5ed4f2fd4c7ca7c126553081071c94a954abc
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2024-03-04 11:36:10 +0200

    Zip removed and  05. Accordion. compleated

--- 02. Asynchronous Programming/Exercise/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
+++ 02. Asynchronous Programming/Exercise/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
@@ -3,6 +3,7 @@ function attachEvents() {
     let getWhetherButton = document.getElementById("submit");
     let forecastDiv = document.getElementById("forecast");
 
+
     getWhetherButton.addEventListener("click", getCity);
     let url = 'http://localhost:3030/jsonstore/forecaster/locations';
     let urlCurrent = `http://localhost:3030/jsonstore/forecaster/today/`
@@ -14,12 +15,11 @@ function attachEvents() {
     }
 
     let symbols = {
-        "Sunny": "&#x2600",
-        " Partly Sunny": "&#x26C5",
-        "Overcast": "&#x2601",
-        "Rain": "&#x2614",
-        "Degrees": "&#176"
-
+        "Sunny": "&#x2600;", 
+        "Partly sunny": "&#x26C5;",
+        "Overcast": "&#x2601;",
+        "Rain": "&#x2614;",
+        "Degrees": "&#176;" 
     }
 
 
@@ -33,7 +33,7 @@ function attachEvents() {
             currentConditions(currentCityWhether.code);
             forecast(currentCityWhether.code);
             forecastDiv.style = "display:block";
-            createForecastToday(data);
+            // createForecastToday(data);
             
 
             // let divClassEl=document.createElement("div class")
@@ -48,25 +48,90 @@ function attachEvents() {
     }
 
     function createForecastToday(data){
-        const container=document.createElement("div");
-        container.classList.add("forecast");
-        const conditionaSpan=document.createElement("span");
-        conditionaSpan.classList.add("condition");
-        conditionaSpan.classList.add("symbol");
+        const currentDiv = document.getElementById('current');
+        currentDiv.innerHTML = ''; // Clear existing content
+    
+        const forecastsDiv = document.createElement('div');
+        forecastsDiv.className = 'forecasts';
+    
+        const symbolSpan = document.createElement('span');
+        symbolSpan.className = 'condition symbol';
+        symbolSpan.innerHTML = symbols[data.forecast.condition] || '';
+        const conditionSpan=document.createElement('span');
+        conditionSpan.className="condition";
+
+        const cityNameSpan=document.createElement('span');
+        cityNameSpan.className="forecast-data";
+        cityNameSpan.textContent=data.name;
+
+        const degreeSpan=document.createElement('span');
+        degreeSpan.className="forecast-data";
+        degreeSpan.innerHTML=`${data.forecast.low}${symbols.Degrees}/${data.forecast.high}${symbols.Degrees}`;
+
+        const whetherSpan=document.createElement('span');
+        whetherSpan.className="forecast-data";
+        whetherSpan.textContent=data.forecast.condition ;
+        
+        conditionSpan.appendChild(cityNameSpan);
+        conditionSpan.appendChild(degreeSpan);
+        conditionSpan.appendChild(whetherSpan);
+    
+        forecastsDiv.appendChild(symbolSpan);
+        forecastsDiv.appendChild(conditionSpan);
+    
+        currentDiv.appendChild(forecastsDiv);  
+         
+    }
+
+    function createThreeDayForecast(data){
+        const upcomingDiv = document.getElementById('upcoming');
+        upcomingDiv.innerHTML = ''; // Clear existing content
+
+        let forecastInfoDiv=document.createElement("div");
+        forecastInfoDiv.className="forecast-info";
+        
+        for(let i=0; i<data.forecast.length;i++){
+            let currect=data.forecast[i];
+            let spanClassUpcoming=document.createElement("span");
+            spanClassUpcoming.className="upcoming";
+    
+            let spanClassSymbol=document.createElement("span");
+            spanClassSymbol.className="symbol";
+            spanClassSymbol.innerHTML=symbols[currect.condition] || '';
+
+
+            let spanClassdegree=document.createElement("span");
+            spanClassdegree.className="forecast-data";
+            spanClassdegree.innerHTML=`${currect.low}${symbols.Degrees}/${currect.high}${symbols.Degrees}`;
+        
+            let spanClasCondition=document.createElement("span");
+            spanClasCondition.className="forecast-data";
+            spanClasCondition.textContent=currect.condition ;
+
+            spanClassUpcoming.appendChild(spanClassSymbol);
+            spanClassUpcoming.appendChild(spanClassdegree);
+            spanClassUpcoming.appendChild(spanClasCondition);
+            forecastInfoDiv.appendChild(spanClassUpcoming)
+            upcomingDiv.appendChild(forecastInfoDiv);
+
+        }
         
 
-    
+
+     
     }
 
     async function currentConditions() {
         const response = await fetch(urlCurrent + currentCityWhether.code);
         const data = await response.json();
+        createForecastToday(data)
 
     }
 
     async function forecast() {
         const response = await fetch(urlUpcoming + currentCityWhether.code);
         const data = await response.json();
+        createThreeDayForecast(data);
     }
 
 }
--- 02. Asynchronous Programming/Exercise/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
+++ 02. Asynchronous Programming/Exercise/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
@@ -1,3 +1,60 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    let url = `http://localhost:3030/jsonstore/advanced/articles/list`;
+    let main = document.getElementById("main")
+    const response = await fetch(url);
+    const data = await response.json();
+    data.forEach(item => {
+        const divAcc = document.createElement("div");
+        divAcc.className = "accordion";
+        const divHead = document.createElement("div");
+        divHead.className = "head";
+        const spanName = document.createElement("span");
+        spanName.textContent = item.title;
+        const button = document.createElement("button");
+        button.className = "button";
+        button.textContent = "More";
+        button.id = item._id;
+
+        button.addEventListener("click", moreFunc);
+
+
+        const divExtra = document.createElement("div");
+        divExtra.className = "extra";
+        const pElement = document.createElement("p");
+        divExtra.appendChild(pElement);
+
+        divHead.appendChild(spanName);
+        divHead.appendChild(button);
+        divAcc.appendChild(divHead);
+        divAcc.appendChild(divExtra);
+        main.appendChild(divAcc)
+
+    });
+
+    async function moreFunc(e) {
+        const button = e.target;
+        const buttonId = e.target.id;
+        const accordion = e.target.parentElement.parentElement;
+        const extraDiv = accordion.children[1];
+        const p = extraDiv.children[0];
+
+        if (button.textContent == "More") {
+            extraDiv.style = "display:block";
+            button.textContent = "Less";
+
+            const response = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${buttonId}`)
+            const data = await response.json();
+            p.textContent = data.content;
+
+        } else if (button.textContent == "Less") {
+            extraDiv.style = "display:none";
+            button.textContent = "More";
+        }
+
+    }
+
+
+
+
 }
+solution();
